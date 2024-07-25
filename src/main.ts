85ae09599b2642adaf6ee1a8884d87dcdc769b00
SergioGDev <admin@sergiogdev.com>
2024-07-25 18:06:22 +0200

    feat: log added

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,8 @@ async function bootstrap() {
   
   await app.listen(envs.port);
 
+  logger.log('Health Check Configured');
   logger.log(`Payments Microservice running on port ${envs.port}`);
+  
 }
 bootstrap();
